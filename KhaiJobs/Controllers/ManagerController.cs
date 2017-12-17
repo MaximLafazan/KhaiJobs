e7e9cdcaba374a34be80d594df436440c90e0c6d
kirill <kirill.kurinnoy@gmail.com>
2017-12-17 20:01:08 +0200

    Добавил менеджер, исправления студент, компетенции..

---
+++ KhaiJobs/Controllers/ManagerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhaiJobs.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListTeachers()
+        {
+            return View();
+        }
+
+        public ActionResult AddTeacher()
+        {
+            return View();
+        }
+        public ActionResult ResumeSearch()
+        {
+            return View();
+        }
+        public ActionResult JobSearch()
+        {
+            return View();
+        }
+        public ActionResult ViewJobPosting()
+        {
+            return View();
+        }
+        public ActionResult ViewResume()
+        {
+            return View();
+        }
+
+    }
+}
