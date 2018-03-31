d52a1beade40cc49743e41ba1e0bdaaaa90efccc
kirill kurinnoy <kirill.kurinnoy@gmail.com>
2018-03-31 13:22:20 +0300

    Revert "Revert "Добавил интефейс компетенций и страницу профиля преподавателя""
    This reverts commit 994d3cd02e7b38c2208cefc6eb10f3cb100f8370.

---
+++ KhaiJobs/Controllers/TeacherController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhaiJobs.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult JobSearch()
+        {
+            return View();
+        }
+        public ActionResult ResumeSearch()
+        {
+            return View();
+        }
+        public ActionResult ViewJobPosting()
+        {
+            return View();
+        }
+        public ActionResult ViewResume()
+        {
+            return View();
+        }
+        public ActionResult ViewProfile()
+        {
+            return View();
+        }
+    }
+}
